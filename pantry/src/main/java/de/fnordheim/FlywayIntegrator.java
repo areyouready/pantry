bea34119df75bd0ddce02cfa28393885dc863add
Sebastian Basner <sebastian.basner@gmx.de>
2017-05-26 00:39:47 +0200

    Cleanup and style refinements

--- pantry/src/main/java/de/fnordheim/FlywayIntegrator.java
+++ pantry/src/main/java/de/fnordheim/FlywayIntegrator.java
@@ -21,48 +21,44 @@ import java.sql.SQLException;
  */
 public class FlywayIntegrator implements Integrator {
 
-    private final Logger log = LoggerFactory.getLogger(FlywayIntegrator.class);
-
-    @Override
-    public void disintegrate(SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
-        // Nothing to do here
-    }
+    private final Logger logger = LoggerFactory.getLogger(FlywayIntegrator.class);
 
     @Override
     public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactory,
                           SessionFactoryServiceRegistry serviceRegistry) {
 
-        log.info("Starting DB migration");
+        logger.info("Migrating database to the latest version");
 
-        Connection connection = null;
         final JdbcServices jdbcServices = serviceRegistry.getService(JdbcServices.class);
+        Connection connection;
+        DataSource dataSource = null;
+
         try {
             connection = jdbcServices.getBootstrapJdbcConnectionAccess().obtainConnection();
-        } catch (SQLException e) {
+            final Method method = connection != null ? connection.getClass().getMethod("getDataSource", (Class<?>) null) : null;
+            dataSource = (DataSource) (method != null ? method.invoke(connection, (Object) null) : null);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException | SQLException e) {
             e.printStackTrace();
         }
-            DataSource dataSource = null;
-            try {
-                final Method method = connection != null ? connection.getClass().getMethod("getDataSource", null) : null;
-                dataSource = (DataSource) (method != null ? method.invoke(connection, null) : null);
 
-            } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
 
-            Flyway flyway = new Flyway();
-            flyway.setDataSource(dataSource);
+        MigrationInfo migrationInfo = flyway.info().current();
 
-            MigrationInfo current = flyway.info().current();
-            if (current == null) {
-                log.info("No existing DB found");
-            }
-            else {
-                log.info("Current DB version is {}", current.getVersion());
-            }
+        if (migrationInfo == null) {
+            logger.info("No existing database at the actual datasource");
+        }
+        else {
+            logger.info("Found a database with the version: {}", migrationInfo.getVersion());
+        }
 
-            flyway.migrate();
+        flyway.migrate();
+        logger.info("Successfully migrated to database version: {}", flyway.info().current().getVersion());
+    }
 
-            log.info("Completed DB migration to version {}", flyway.info().current().getVersion());
+    @Override
+    public void disintegrate(SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
+        // Not needed here
     }
 }
